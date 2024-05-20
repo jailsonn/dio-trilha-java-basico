c2a10250bbc638f5830cf61fca45647fd3b1d7e8
jailsonn <jailson.nogueira2@gmail.com>
2024-05-20 01:47:49 -0300

    add operadores

--- tipos-variaveis/src/OperadoresLogicos.java
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -1,7 +1,8 @@
 public class OperadoresLogicos {
     // Operadores lógicos
     // (e, End, &&) Todos os valores precisam serem verdadeiros
-    // (OU, OR, ||) Apenas um ou mais de um, (porem nao toddos), valor precisa ser verdadeiro
+    // (OU, OR, ||) Apenas um ou mais de um, (porem nao toddos), valor precisa ser
+    // verdadeiro
     // (Not !) Operador de negação, inverte o valor da operação
 
     // Babela verdade dos Operadores Lógicos
@@ -24,5 +25,16 @@ public class OperadoresLogicos {
     // Operador de Atribuição
     // (=)
 
-    
+    public static void main(String[] args) {
+        int nota = 10;
+        // int trabalho = 7;
+
+        if (nota >= 6 && nota <= 10) {
+            System.out.println("Aluno: Aprovado");
+        } else if (nota > 10 || nota < 0) {
+            System.out.println("Esta Nota não Existe");
+        } else {
+            System.out.println("Aluno: Reprovado");
+        }
+    }
 }
