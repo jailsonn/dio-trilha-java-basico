36072c387df7f93406469520eeaf5ee1fc38921d
jailsonn <jailson.nogueira2@gmail.com>
2024-05-22 01:17:48 -0300

    add metodos

---
+++ tipos-variaveis/src/edu/jailson/escopo/Escopo.java
@@ -0,0 +1,14 @@
+package edu.jailson.escopo;
+
+public class Escopo {
+    // 3 Tipos de escopos iniciais
+    // Escopo de Classe
+    // Escopo de método
+    // Escopo de fluxo
+
+    // Escopo é o ambiente onde uma variável pode ser acessada
+    // Obs. se vc declarar uma variável no corpo da classe (ela vai está disponivel para todos)
+    // e se vc declarar uma variável dentro de um método(Só vai está disponivel para o método)
+
+    
+}
