c7ecced9e5e77f2e8d1815ce80a8c728b05a676f
jailsonn <jailson.nogueira2@gmail.com>
2024-05-23 21:21:57 -0300

    escopos

---
+++ tipos-variaveis/src/edu/jailson/comentarios/Comentarios.java
@@ -0,0 +1,17 @@
+package edu.jailson.comentarios;
+
+public class Comentarios {
+    // Comentários são importantes para documentar trechos do código
+
+    // Exitem dois tipos de comentários (de uma linha e o de multiplas linhas)
+    // Exemplos:
+    // Comentario de uma linha reprezentado por (// ou Ctrl+; na IDE VsCode Windows)
+    // Comentário de multiplas linhas ou varias (/** */ ou shift+Alt+A)
+
+    // Exemplo:
+    /* Esse 
+     * é
+     * um comentário de varias linhas ok.
+     */
+    
+}
---
+++ tipos-variaveis/src/edu/jailson/palavras.reservadas/PalavrasReservadas.java
@@ -0,0 +1,20 @@
+
+
+public class PalavrasReservadas {
+    // 52 palavras reservadas da linguagem JAVA
+    // Todas as palavras reservadas são em MINUSCULA
+    // não podem ser utilizadas para nomear, classes, atributos, ou métdos
+
+    // Tipos Primitivos
+    // Classificações
+    // Escopo de uso
+    // Palavras "opostas"
+
+    // MODIFICADORES DE ACESSO
+    // Public: acesso de qualquer classe
+    // private: acesso apenas dentro da classe
+    // Protected: acesso por classes no mesmo pacote e subclasses(Herança)
+
+
+    
+}
