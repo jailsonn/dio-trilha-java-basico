92f5df30b256493e6560ffe6fb74b25b5ff3dc3e
jailsonn <jailson.nogueira2@gmail.com>
2024-05-23 22:24:20 -0300

    comentarios

---
+++ tipos-variaveis/src/edu/jailson/modificadoresAcesso/ModificadoresAcesso.java
@@ -0,0 +1,12 @@
+package edu.jailson.modificadoresAcesso;
+
+public class ModificadoresAcesso {
+    // Os modificadores de acessos, para 
+
+
+    // MODIFICADORES DE ACESSO
+    // Public: acesso de qualquer classe
+    // private: acesso apenas dentro da classe
+    // Protected: acesso por classes no mesmo pacote e subclasses(Herança)
+    
+}
